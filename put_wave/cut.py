c5d1de809ffb83faffb3934feae2de962a6f8d0e
Arseney Abraamyan <ar.abraamyan@gmail.com>
2020-08-25 12:08:45 +0300

    clean code

--- put_wave/cut.py
+++ put_wave/cut.py
@@ -1,16 +1,14 @@
 # https://linuxize.com/post/how-to-install-ffmpeg-on-ubuntu-18-04/
 
-# на сервак надо накатить ffmpeg
+# на машину надо накатить ffmpeg
 
 from pydub import AudioSegment
-# from pydub.utils import which
 
-# AudioSegment.converter = which("ffmpeg")
-sound = AudioSegment.from_file("/var/www/scripts/put_wave/11111.wav", format="mp3")
+sound = AudioSegment.from_file("/var/www/scripts/put_wave/source.wav", format="mp3")
 
 
 duration = len(sound)   
 
 extract = sound[:8500]
 print(duration)
-extract.export('11111111-extract.mp3', format="mp3")
+extract.export('source-extract.mp3', format="mp3")
